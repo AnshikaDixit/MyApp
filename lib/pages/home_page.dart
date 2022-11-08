f3e49a7f9a9604fc8eca424af305b625b6aebc3c
Anshika Dixit <dixitanshika09@gmail.com>
2022-11-08 16:55:21 +0530

    list view with dummy data

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,29 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:myfirstapp/widgets/drawer.dart';
+import '../models/catalog.dart';
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    int app = 15;
-    String name = "Anshika";
-
+    final dummyList = List.generate(20, (index) => CatalogModel.item[0]);
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Catalog App",
-          style: TextStyle(color: Colors.black,
-          )),
+        title: const Text("Catalog App",
+            style: TextStyle(
+              color: Colors.black,
+            )),
       ),
-      body: Center(
-        child: Container(
-          child: Text("welcome to my $app app by $name!"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
-      drawer: const Drawer(
-        child: MyDrawer(),
-      ),
+      drawer: const MyDrawer(),
     );
   }
 }
